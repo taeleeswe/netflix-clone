ee39c389b7bf4588306f0ebb9b4a0465d23f7dca
Tae Young Lee <taeleeswe@gmail.com>
2023-05-15 15:27:15 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    ".app/**/*.{js,ts,jks,tsx}",
+    "./pages/**/*.{js,ts,jks,tsx}",
+    ".components/**/*.{js,ts,jks,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
